fbe48013b31e47754e2c47302b5607947b4b8bb2
Mahesh Kumar Behera <mahesh@apache.org>
2019-12-04 15:25:29 +0530

    HOTFIX-2880 BUG-122980 ENGESC-117 : HWC executeUpdate not throwing exception
    Change-Id: Ic6fda125bd96871209ef307265f40f5b3522de5e
    (cherry picked from commit 51141870ee91f65d66e47c9dfb69dfcacb6bd641)

--- src/main/java/com/hortonworks/spark/sql/hive/llap/HiveWarehouseSession.java
+++ src/main/java/com/hortonworks/spark/sql/hive/llap/HiveWarehouseSession.java
@@ -56,6 +56,10 @@ public interface HiveWarehouseSession {
 
     boolean executeUpdate(String sql);
 
+    /**
+     * @deprecated  Use {@link #executeUpdate(String sql)}
+     */
+    @Deprecated
     boolean executeUpdate(String sql, boolean propagateException);
 
     Dataset<Row> table(String sql);
